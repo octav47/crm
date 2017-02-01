98906c2130b0183a5bede0a2b4cf2dd23295b164
tribunskiy <forty-7@yandex.ru>
2017-02-01 17:02:19 +0300

    Добавлены группы

--- app-config/main.js
+++ app-config/main.js
@@ -1,9 +1,18 @@
 A.app({
     appName: 'CRM DD',
     appIcon: 'phone',
-    onlyAuthenticated: true,
+    onlyAuthenticated: false,
     menuItems: [
+        // {
+        //     name: 'Преподаватели',
+        //     entityTypeId: 'Teacher',
+        //     icon: 'user'
+        // },
         {
+            name: 'Группы',
+            entityTypeId: 'Group',
+            icon: 'user'
+        }, {
             name: 'Клиенты',
             entityTypeId: 'Client',
             icon: 'user'
@@ -32,6 +41,22 @@ A.app({
     ],
     entities: function (Fields) {
         return {
+            // Teacher: {
+            //     fields: {
+            //         name: Fields.text('ФИО').required(),
+            //         phone: Fields.text('Телефон'),
+            //         groups: Fields.reference('Группы', 'Group')
+            //     },
+            //     referenceName: 'name'
+            // },
+            Group: {
+                fields: {
+                    name: Fields.text('Название').required()
+                    // clients: Fields.relation('Ученики', 'Client', 'group'),
+                    // teacher: Fields.relation('Преподаватели', 'Teacher', 'groups')
+                },
+                referenceName: 'name'
+            },
             Client: {
                 fields: {
                     name: Fields.text('ФИО').required(),
@@ -43,7 +68,8 @@ A.app({
                     // subscription: Fields.multiReference('Абонементы', 'Subscription'),
                     status: Fields.checkbox('Активен'),
                     purchases: Fields.relation('Покупки', 'Purchase', 'client'),
-                    bonus: Fields.integer('Бонусы').computed('sum(purchases.bonus)')
+                    bonus: Fields.integer('Бонусы').computed('sum(purchases.bonus)'),
+                    group: Fields.multiReference('Группы', 'Group', 'clients')
                 },
                 views: {
                     Client: {
@@ -66,6 +92,7 @@ A.app({
                     expireDate: Fields.date('Дата окончания'),
                     client: Fields.reference('Клиент', 'Client').required(),
                     subscription: Fields.reference('Покупка', 'Subscription').required(),
+                    group: Fields.reference('Группа', 'Group'),
                     value: Fields.integer('Стоимость').readOnly(),
                     bonus: Fields.integer('Бонус за покупку').readOnly()
                 },
